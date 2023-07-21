5e16c61bc7c7a8c25fa4b4b652b60c973e03a9f5
minh352623 <congminh352623@gmail.com>
2023-07-21 20:45:58 +0700

    function netify

--- src/pages/client/Contact.jsx
+++ src/pages/client/Contact.jsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 
-import * as mobilenet from "@tensorflow-models/mobilenet";
+// import * as mobilenet from "@tensorflow-models/mobilenet";
 
 const HeaderClient = () => {
 
@@ -16,36 +16,36 @@ const HeaderClient = () => {
     const fileInputRef = React.useRef()
 
     const loadModel = async () => {
-        setIsModelLoading(true)
-        try {
-            const model = await mobilenet.load()
-            setModel(model)
-            setIsModelLoading(false)
-        } catch (error) {
-            console.log(error)
-            setIsModelLoading(false)
-        }
+        // setIsModelLoading(true)
+        // try {
+        //     const model = await mobilenet.load()
+        //     setModel(model)
+        //     setIsModelLoading(false)
+        // } catch (error) {
+        //     console.log(error)
+        //     setIsModelLoading(false)
+        // }
     }
 
     const uploadImage = (e) => {
-        const { files } = e.target
-        if (files.length > 0) {
-            const url = URL.createObjectURL(files[0])
-            setImageURL(url)
-        } else {
-            setImageURL(null)
-        }
+        // const { files } = e.target
+        // if (files.length > 0) {
+        //     const url = URL.createObjectURL(files[0])
+        //     setImageURL(url)
+        // } else {
+        //     setImageURL(null)
+        // }
     }
 
     const identify = async () => {
-        textInputRef.current.value = ''
-        const results = await model.classify(imageRef.current)
-        setResults(results)
+        // textInputRef.current.value = ''
+        // const results = await model.classify(imageRef.current)
+        // setResults(results)
     }
 
     const handleOnChange = (e) => {
-        setImageURL(e.target.value)
-        setResults([])
+        // setImageURL(e.target.value)
+        // setResults([])
     }
 
     const triggerUpload = () => {
@@ -53,13 +53,13 @@ const HeaderClient = () => {
     }
 
     React.useEffect(() => {
-        loadModel()
+        // loadModel()
     }, [])
 
     React.useEffect(() => {
-        if (imageURL) {
-            setHistory([imageURL, ...history])
-        }
+        // if (imageURL) {
+        //     setHistory([imageURL, ...history])
+        // }
     }, [imageURL])
 
     if (isModelLoading) {
