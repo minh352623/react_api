a8d971c82056eb235f1e3bb841978b286e1179ad
minh352623 <congminh352623@gmail.com>
2023-09-03 23:18:06 +0700

    tessss

--- src/pages/client/Blockchain.jsx
+++ src/pages/client/Blockchain.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import  { useEffect, useState } from "react";
 import Layout from "../../components/layouts/Layout";
 import { ethers } from "ethers";
 import { ABI, address } from "../../trait/ABI";
