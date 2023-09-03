da5b1c4bd77dc15832eae30541a9f05701073fe7
minh352623 <congminh352623@gmail.com>
2023-09-03 23:04:03 +0700

    tour metamask

--- src/main.jsx
+++ src/main.jsx
@@ -8,14 +8,66 @@ import store from "./redux-thunk/configureStore";
 import { Provider } from "react-redux";
 import { createRoot } from "react-dom/client";
 import "./common/axiosSetup.js";
+import { TourProvider } from "@reactour/tour";
 const container = document.getElementById("root");
 const root = createRoot(container);
+
+const tourConfig = [
+  {
+    selector: '[data-tut="reactour__iso"]',
+    content: () => (
+      <div className="flex flex-col gap-3" >
+        <img src="./abc4.png" alt="" />
+      </div>
+    ),
+    position: "right"
+
+  },
+  {
+    selector: '[data-tut="reactour__style"]',
+    content: () => (
+      <div className="flex flex-col gap-3" >
+        <img src="./abc5.png" alt="" />
+    </div>
+    ),
+    style: {
+      backgroundColor: "black",
+      color: "white"
+    }
+  },
+  {
+    selector: '[data-tut="reactour__goTo"]',
+    content: ({ goTo }) => (
+      <div className="flex flex-col gap-3" >
+      <img src="./abc6.png" alt="" />
+  </div>
+    )
+  },
+  {
+    selector: '[data-tut="reactour__position"]',
+    content: () => (
+      <div className="flex flex-col gap-3" >
+      <img src="./abc8.png" alt="" />
+  </div>
+    ),
+    position: "left"
+  }
+];
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </Provider>
+   
+      <Provider store={store}>
+        <BrowserRouter>
+        <TourProvider
+    steps={tourConfig}
+    // maskClassName="mask"
+    // className="helper"
+    // rounded={5}
+ 
+  >
+          <App />
+            </TourProvider>
+        </BrowserRouter>
+      </Provider>
   </React.StrictMode>
 );
--- src/pages/client/Blockchain.jsx
+++ src/pages/client/Blockchain.jsx
@@ -2,8 +2,11 @@ import React, { useEffect, useState } from "react";
 import Layout from "../../components/layouts/Layout";
 import { ethers } from "ethers";
 import { ABI, address } from "../../trait/ABI";
+import { TourProvider, useTour } from '@reactour/tour'
 
 const Blockchain = () => {
+  const accentColor = "#5cb7b7";
+
   const [provider, setProvider] = useState(null);
   const [singer, setSinger] = useState(null);
   const [contract, setContract] = useState(null);
@@ -13,7 +16,9 @@ const Blockchain = () => {
   const [inputEther, setInputEther] = useState(0);
   const [stateEnter, setStateEnter] = useState("");
   const [statePickWInner, setStatePickWInner] = useState("");
-
+  const [openTourS,setOpenTour] = useState(false);
+  const [isShowingMore,setisShowingMore] = useState(false);
+  const { setIsOpen } = useTour();
 
   const initializeProvider = async () => {
     if (window.ethereum) {
@@ -69,13 +74,14 @@ const Blockchain = () => {
   const enterETH = async (e) => {
     e.preventDefault();
     try{
-      setStatePickWInner("Waiting on transition success... ");  
+      setStateEnter("Waiting on transition success... ");  
       const accounts = await provider.send("eth_requestAccounts", []);
-      await contract.enter({
+      const res= await contract.enter({
         from: accounts[0],
         value:ethers.utils.parseEther(inputEther)
       })
-      setStatePickWInner("You have been entered");  
+      console.log("🚀 ~ file: Blockchain.jsx:78 ~ enterETH ~ res:", res)
+      setStateEnter("You have been entered");  
 
     }catch(err){
       console.log("🚀 ~ file: Blockchain.jsx:69 ~ enterETH ~ err:", err)
@@ -84,20 +90,37 @@ const Blockchain = () => {
  
   };
   const pickWinner = async (e) => {
+    setStatePickWInner("Waiting on pick winner success... ");  
+
     const accounts = await provider.send("eth_requestAccounts", []);
-    await contract.pickWinner({
+    const res = await contract.pickWinner({
       from: accounts[0],
     })
+    console.log("🚀 ~ file: Blockchain.jsx:94 ~ pickWinner ~ res:", res)
+    setStatePickWInner("Pick winner success");  
+
   }
   useEffect(() => {
     initializeProvider();
   }, []);
+  
+  const closeTour = () => {
+    setIsOpen(false);
+  };
 
+  const openTour = () => {
+    setIsOpen(true);
+
+  };
+
+  
   return (
+    <>
     <Layout>
       <div className="m-auto flex justify-start items-start gap-3 flex-col p-5">
         <div className="flex gap-3 justify-start  items-start ">
           <button
+          data-tut="reactour__position"
             className="px-3 py-2 rounded-xl bg-orange-500 text-white"
             onClick={initializeProvider}
           >
@@ -121,6 +144,12 @@ const Blockchain = () => {
           >
             Get Balance
           </button>
+          <button
+            className="px-3 py-2 rounded-xl bg-slate-500 text-white"
+            onClick={openTour}
+          >
+            Help
+          </button>
         </div>
         <div className="flex flex-col justify-start items-start">
           <p>Address manager: {manager}</p>
@@ -135,7 +164,7 @@ const Blockchain = () => {
           <hr />
         </div>
         <div className="flex flex-col gap-3">
-          <h3>Want to try your luck?</h3>
+          <h3 data-tut="reactour__iso">Want to try your luck?</h3>
           <form onSubmit={enterETH} action="">
             <div className="flex flex-col gap-3">
               <label htmlFor="">Amount of ETH to enter</label>
@@ -145,6 +174,7 @@ const Blockchain = () => {
                 onChange={(e) => setInputEther(e.target.value)}
               />
               <button
+              data-tut="reactour__style"
                 className="px-3 py-2 rounded-xl bg-blue-500 text-white"
                 // onClick={enterETH}
               >
@@ -154,16 +184,23 @@ const Blockchain = () => {
           </form>
           <h1>{stateEnter}</h1>
           <button
+            data-tut="reactour__goTo"
             className="px-3 py-2 rounded-xl bg-orange-500 text-white"
             onClick={pickWinner}
           >
             PickWiner
           </button>
-          <h1>{stateEnter}</h1>
+          <h1>{statePickWInner}</h1>
         </div>
       </div>
     </Layout>
+
+    </>
   );
 };
+
+
+
+
 
 export default Blockchain;
